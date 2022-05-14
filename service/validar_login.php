5032992f713e66191df6276a3e5152d78dd735eb
Leandro Souza Ferreira <57051797+OInenarravel@users.noreply.github.com>
2022-05-13 22:59:32 -0300

    Add files via upload

---
+++ service/validar_login.php
@@ -0,0 +1,25 @@
+<?php
+require_once("../models/Sessao.php");
+
+$oSessao = new Sessao();
+
+$sLogin = $_POST['login'];
+$sSenha = $_POST['senha'];
+
+//impossibilitando SQL Injection
+$sLogin = addslashes($sLogin);
+$sSenha = addslashes($sSenha);
+
+$loginValido = $oSessao->efetuarLogin($sLogin, $sSenha);
+
+if ($loginValido) {
+    header("Location: ../portal/index.php");
+    exit;
+} else {
+    echo "
+    <script type='text/javascript'>
+        alert('Erro ao logar! Confira seu login e senha e tente novamente.');
+        document.location = '../index.php';
+    </script>";
+}
+?>
